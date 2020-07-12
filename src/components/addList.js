082a8ceaa8830e840c42ebe6f72cb9ab236b6512
flormcalvo <flor.calvo89@gmail.com>
2020-07-12 15:22:32 +1000

    Create AddList component

--- src/components/addList.js
+++ src/components/addList.js
@@ -2,15 +2,31 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Container = styled.article`
+  font-family: inherit;
   gap: 0.5rem;
   border-radius: 0.2rem;
   background: #ebecf0;
   padding: 0.4rem 0 0.2rem 0;
   margin: 0.3rem 0;
+  &:hover {
+    cursor: pointer;
+    background: rgba(115, 116, 117, 0.3);
+  }
+`;
+const Paragraph = styled.p`
+  width: 15rem;
+  margin: 0.2 0rem;
+  padding: 0.3rem;
+  border-radius: 0.1875rem;
+  color: #737475;
 `;
 
 const AddList = () => {
-  return <Container>hollo</Container>;
+  return (
+    <Container>
+      <Paragraph>+Add New List</Paragraph>
+    </Container>
+  );
 };
 
 export default AddList;
