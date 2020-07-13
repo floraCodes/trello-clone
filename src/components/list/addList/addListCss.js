133731983381fbec21a07723aaeb26e9c8cdd1b6
flormcalvo <flor.calvo89@gmail.com>
2020-07-13 17:23:21 +1000

    Refactor Styles

--- src/components/list/addList/addListCss.js
+++ src/components/list/addList/addListCss.js
@@ -6,7 +6,7 @@ export const Container = styled.article`
   border-radius: 0.2rem;
   background: #ebecf0;
   padding: 0.4rem 0 0.2rem 0;
-  margin: 0.3rem 0;
+  margin: 0.3rem 0.1rem;
   &:hover {
     cursor: pointer;
     background: rgba(115, 116, 117, 0.3);
--- src/components/list/listCss.js
+++ src/components/list/listCss.js
@@ -1,9 +1,8 @@
 const { default: styled } = require('@emotion/styled');
 
 export const Container = styled.article`
-  gap: 0.5rem;
   border-radius: 0.2rem;
   background: #ebecf0;
   padding: 0.4rem 0 0.2rem 0;
-  margin: 0.3rem 0;
+  margin: 0.3rem 0.1rem;
 `;
